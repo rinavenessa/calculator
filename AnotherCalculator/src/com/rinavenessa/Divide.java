8495f8042f38bd58499399404ea17221fff6a717
Zibusiso Bhango <zbhango@gmail.com>
2020-04-28 17:56:55 +0200

    code review for calculator2. Venessa added OOP and interacted with objects to calculate the mathematical functions.

--- AnotherCalculator/src/com/rinavenessa/Divide.java
+++ AnotherCalculator/src/com/rinavenessa/Divide.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 
 public class Divide {
     public static void divide(){
+		/**
+    	 * This is a huge improvement from Calculator1, well done! Calculator1 was a software developer's
+    	 * solution. This is a software engineer's solution, its absolutely brilliant. Well done on OOP.
+    	 * 
+    	 * The idea is to have interaction with the user in a single focal point of the project, which 
+    	 * is the main method in the Main class for this project. Notice that you are creating the variables 
+    	 * firstNumber, secondNumber and answer in all your classes and asking the user for input in all 
+    	 * classes as well. The asking of user input must be done in the main method in the Main class.
+    	 * 
+    	 * My advice? Make your classes return integers (or double or float for division - or you could
+    	 * leave it as is for integer division). I wonder if you are aware that 5/2 gives you 2, and not 2.5
+    	 * as expected? Also, pass firstNumber and secondNumber as parameters for the class, and add them to
+    	 * the class when you call it from the instantiated object in main method. Thabi implemented something
+    	 * similar to this on the solution I asked him to share with you.
+    	 * 
+    	 * Again, this is great work, and your solution works as expected. But I want to direct you on the OOP
+    	 * way of doing things as you requested. Let me know how this goes.
+    	 */
         Scanner input3 = new Scanner(System.in);
         System.out.println("enter first number:");
         int firstNumber = input3.nextInt();
--- AnotherCalculator/src/com/rinavenessa/Main.java
+++ AnotherCalculator/src/com/rinavenessa/Main.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+		 // please try to enact a menu system with numbers here. It simplifies the process for the user.
         Scanner sign = new Scanner(System.in);
         System.out.println("choose operator between + - * / ");
         System.out.println("enter operator:");
         char operator = sign.next().charAt(0);
-
+		// well done on instantiating the objects here, looks good :)
         Add obj = new Add();
         Subtract obj1 = new Subtract();
         Multiply obj2 = new Multiply();
         Divide obj3 = new Divide();
 
 
-
+		// nice touch with the switch statement!
         switch (operator){
             case '+':
                 obj.add();
@@ -35,6 +35,7 @@ public class Main {
 
             default:
                 System.out.println("Enter a valid operator.");
+			// also, please alert the user of the answer. A number just displays without context of what it is after entering firstNumber and secondNumber.
         }
     }
 }
