b8c10be107df958072ada41a1d039c83392f2587
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-04-30 12:24:59 +0200

    Update Add.java

--- AnotherCalculator/src/com/rinavenessa/Add.java
+++ AnotherCalculator/src/com/rinavenessa/Add.java
@@ -6,9 +6,9 @@ public class Add {
     public static void add(){
         Scanner input = new Scanner(System.in);
         System.out.println("enter first number:");
-        double firstNumber = input.nextInt();
+        double firstNumber = input.nextDouble();
         System.out.println("enter second number");
-        double secondNumber = input.nextInt();
+        double secondNumber = input.nextDouble();
         double answer = firstNumber +  secondNumber;
         System.out.println(answer);
     }
