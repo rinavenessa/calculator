ab7715b6cb0461b9c54e30d31933ec6b06363384
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-04-30 12:19:47 +0200

    Update Multiply.java

--- AnotherCalculator/src/com/rinavenessa/Multiply.java
+++ AnotherCalculator/src/com/rinavenessa/Multiply.java
@@ -6,10 +6,10 @@ public class Multiply {
     public static void multiply(){
         Scanner input2 = new Scanner(System.in);
         System.out.println("enter first number:");
-        int firstNumber = input2.nextInt();
+        double firstNumber = input2.nextInt();
         System.out.println("enter second number");
-        int secondNumber = input2.nextInt();
-        int answer = firstNumber *  secondNumber;
+        double secondNumber = input2.nextInt();
+        double answer = firstNumber *  secondNumber;
         System.out.println(answer);
     }
 }
