5a45cf04eb3ca63b9d1e0886d3d506f16b35a1a0
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-04-30 12:20:44 +0200

    Update Subtract.java

--- AnotherCalculator/src/com/rinavenessa/Subtract.java
+++ AnotherCalculator/src/com/rinavenessa/Subtract.java
@@ -6,10 +6,10 @@ public class Subtract {
     public static void subtract(){
         Scanner input1 = new Scanner(System.in);
         System.out.println("enter first number:");
-        int firstNumber = input1.nextInt();
+        double firstNumber = input1.nextInt();
         System.out.println("enter second number");
-        int secondNumber = input1.nextInt();
-        int answer = firstNumber -  secondNumber;
+        double secondNumber = input1.nextInt();
+        double answer = firstNumber -  secondNumber;
         System.out.println(answer);
     }
 }
