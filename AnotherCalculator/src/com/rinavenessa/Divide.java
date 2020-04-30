888280a6bcc8739f407bb94cc25ff9255adb87c5
rinavenessa <63901627+rinavenessa@users.noreply.github.com>
2020-04-30 12:15:07 +0200

    Update Divide.java

--- AnotherCalculator/src/com/rinavenessa/Divide.java
+++ AnotherCalculator/src/com/rinavenessa/Divide.java
@@ -6,10 +6,10 @@ public class Divide {
     public static void divide(){
         Scanner input3 = new Scanner(System.in);
         System.out.println("enter first number:");
-        int firstNumber = input3.nextInt();
+        double firstNumber = input3.nextInt();
         System.out.println("enter second number");
-        int secondNumber = input3.nextInt();
-        int answer = firstNumber /  secondNumber;
+        double secondNumber = input3.nextInt();
+        double answer = firstNumber /  secondNumber;
         System.out.println(answer);
     }
 }
